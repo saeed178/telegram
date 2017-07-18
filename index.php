7db07eab2d4f3fe2b22b3f1af2f525a8c838ab13
saeed178 <saeed7htc@gmail.com>
2017-07-18 11:43:21 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 // Load composer
 require __DIR__ . '/vendor/autoload.php';
-echo "heloooooooooooooooooo";
+
 $API_KEY = '447205620:AAGfcb3s9IXBeY28b3h0cP1zpxjXWh7Wavw';
-$BOT_NAME = 'sirjanbot';
+$BOT_NAME = 'sirjanBot';
+$hook_url = 'https://yourdomain/path/to/hook.php';
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
 
-    // Handle telegram webhook request
-    $telegram->handle();
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    // Silence is golden!
-    // log telegram errors
-    // echo $e;
+    echo $e;
 }

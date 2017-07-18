639b98fd1fae76e747e556f5fb3ca105dce9156a
saeed178 <saeed7htc@gmail.com>
2017-07-18 12:37:38 +0430

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Longman\TelegramBot\Request;
 use Longman\TelegramBot\Telegram;
-
+$message= file_get_contents("php://input");
 $API_KEY = '447205620:AAGfcb3s9IXBeY28b3h0cP1zpxjXWh7Wavw';
 $BOT_NAME = 'sirjanBot';
 
@@ -15,7 +15,7 @@ $telegram = new Telegram($API_KEY, $BOT_NAME);
 
 // Get the chat id and message text from the CLI parameters.
 $chat_id = "358589496";
-$message = $telegram->getLastCommandResponse();
+//$message = $telegram->getLastCommandResponse();
 
 
 if ($chat_id !== '' && $message !== '') {

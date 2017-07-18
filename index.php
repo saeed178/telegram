2b1b4554632b9e8f32b6eb96074655974592e26f
saeed178 <saeed7htc@gmail.com>
2017-07-18 13:02:31 +0430

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,8 @@ $command = $update['message']['text'];
 //$message = "Welcome to Sirjan";
 
  if($command == '/start'){
-    $message= "سلام، به ربات ما خوش آمدید"."<br>"."saeeed7htc@gmail.com";
+    $message= "<br>سلام، به ربات ما خوش آمدید";
+    $message = $message."saeeed7htc@gmail.com";
  }else{
     $message= "دستور شما نا معتبر است";
  }

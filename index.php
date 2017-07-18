4ff6fdf51c556c78ab82ebc256b94fe7eae21ba9
saeed178 <saeed7htc@gmail.com>
2017-07-18 11:58:04 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
-// Load composer
+/**
+ * Usage on CLI: $ php broadcast.php [telegram-chat-id] [message]
+ */
+
 require __DIR__ . '/vendor/autoload.php';
+
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Telegram;
+
 echo "salam heyyyyyyyyyyyyyyyyyyyyyyyyyy";
 $API_KEY = '447205620:AAGfcb3s9IXBeY28b3h0cP1zpxjXWh7Wavw';
 $BOT_NAME = 'sirjanBot';
-$hook_url = 'https://sirjanbot.herokuapp.com/index.php';
-try {
-    // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
 
-    // Set webhook
-    $result = $telegram->setWebhook($hook_url);
+$telegram = new Telegram($API_KEY, $BOT_NAME);
+
+// Get the chat id and message text from the CLI parameters.
+$chat_id = isset($argv[1]) ? $argv[1] : '';
+$message = isset($argv[2]) ? $argv[2] : '';
+
+if ($chat_id !== '' && $message !== '') {
+    $data = [
+        'chat_id' => $chat_id,
+        'text'    => $message,
+    ];
+
+    $result = Request::sendMessage($data);
+
     if ($result->isOk()) {
-        echo $result->getDescription();
+        echo 'Message sent succesfully to: ' . $chat_id;
+    } else {
+        echo 'Sorry message not sent to: ' . $chat_id;
     }
-} catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    echo $e;
 }

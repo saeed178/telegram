d44e45b0b00477dc74c8f669940c34a641e3a71f
saeed178 <saeed7htc@gmail.com>
2017-07-18 12:24:36 +0430

    Update index.php

--- index.php
+++ index.php
@@ -14,11 +14,9 @@ $BOT_NAME = 'sirjanBot';
 $telegram = new Telegram($API_KEY, $BOT_NAME);
 
 // Get the chat id and message text from the CLI parameters.
-//$chat_id = "358589496";
-//$message = "salam my dear";
+$chat_id = "358589496";
+$message = $telegram->getLastCommandResponse();
 
-$chat_id = isset($argv[1]) ? $argv[1] : '';
-$message = isset($argv[2]) ? $argv[2] : '';
 
 if ($chat_id !== '' && $message !== '') {
     $data = [
